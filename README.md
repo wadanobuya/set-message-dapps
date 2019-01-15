1a7e156a6b9e9d9c3a985c9746c21c4b1b775ef9
和田伸也 <wadanobuya1205@gmail.com>
2019-01-15 22:47:26 +0900

    ・remixで実装したコントラクトにアクセス ※実行時にコマンドラインにエラーが表示されるも動作している

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # set-message-dapps
+
+# contractsを実装
+
+remixでcontractsを実装
+http://remix.ethereum.org
+
+# web_appでcontractsのメソッドの取得、登録をする
+
+2019.1.15時点では取得のみ
+
+cd web_app
+npm install
+npm run serve
+
+ブラウザからアクセス
+
+  App running at:
+  - Local:   http://localhost:8080/
+  - Network: http://192.168.100.20:8080/
+
+  Note that the development build is not optimized.
+  To create a production build, run npm run build.
+
+
---
+++ vue-ts-practice/src/components/contractInfo.ts
@@ -0,0 +1,135 @@
+export default {
+  Token: {
+    // Every time you deploy a contract, you have to update address.
+    address: '0xa4ac37f7c15d12d9c57768bad57781ee974d4cbf',
+    // Every time you change the contract interface, you have to update abi.
+    abi: [
+      {
+        constant: false,
+        inputs: [
+          {
+            name: 'to',
+            type: 'address',
+          },
+          {
+            name: 'value',
+            type: 'uint256',
+          },
+        ],
+        name: 'transfer',
+        outputs: [
+          {
+            name: '',
+            type: 'bool',
+          },
+        ],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            name: 'from',
+            type: 'address',
+          },
+          {
+            indexed: true,
+            name: 'to',
+            type: 'address',
+          },
+          {
+            indexed: false,
+            name: 'value',
+            type: 'uint256',
+          },
+        ],
+        name: 'Transfer',
+        type: 'event',
+      },
+      {
+        inputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'constructor',
+      },
+      {
+        constant: true,
+        inputs: [
+          {
+            name: 'owner',
+            type: 'address',
+          },
+        ],
+        name: 'balanceOf',
+        outputs: [
+          {
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'decimals',
+        outputs: [
+          {
+            name: '',
+            type: 'uint8',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'name',
+        outputs: [
+          {
+            name: '',
+            type: 'string',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'symbol',
+        outputs: [
+          {
+            name: '',
+            type: 'string',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'totalSupply',
+        outputs: [
+          {
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ],
+  },
+};
